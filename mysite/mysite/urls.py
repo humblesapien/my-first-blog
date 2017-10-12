06ee7e4e27e3f1ccf2e89c4a846d7f4cf3bcd42d
Kavish <kbgandhi12@gmail.com>
2017-10-12 18:20:41 +0530

    "UrlsChangedforSignup"

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 from django.conf.urls import include,url
 from django.contrib import admin
 from django.contrib.auth import views
-from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-	url(r'^$', views.index, name='index'),
 	url(r'^accounts/login/$', views.login, name='login'),
 	url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 	url(r'',include('blog.urls')),
