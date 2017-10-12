22fc9478f74451f47c30e2c3534f77fb2a3f9604
Kavish <kbgandhi12@gmail.com>
2017-10-12 19:08:37 +0530

    "views"

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^signup/$', views.index, name='index'),
+	url(r'^signup/$', views.signup, name='signup'),
 	url(r'^$' , views.post_list , name = 'post_list'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
