f8f7669fa945490b7306fd20f3c3b94cc4f65261
Kavish <kbgandhi12@gmail.com>
2017-10-12 13:06:09 +0530

    UrlsChanges

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import include,url
 from django.contrib import admin
 from django.contrib.auth import views
+from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
