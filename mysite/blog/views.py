8a5810d3235ac62f5eb9307703787636a679bbbb
Kavish <kbgandhi12@gmail.com>
2017-10-12 19:02:59 +0530

    "Syntax"

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -101,4 +101,4 @@ def signup(request):
             return redirect('login')
     else:
         form = UserCreationForm()
-    return render(request, 'blog/signup.html', {'form': form})=
+    return render(request, 'blog/signup.html', {'form': form})
